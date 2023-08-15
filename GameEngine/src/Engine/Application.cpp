2180652b9ab3f9a0ec8dc47b6375f8ddcb4e08ad
uav <uav0108@gmail.com>
2023-08-15 20:18:44 +0530

    added support for shader compilation

--- GameEngine/src/Engine/Application.cpp
+++ GameEngine/src/Engine/Application.cpp
@@ -45,6 +45,26 @@ namespace Engine {
 		};
 		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
+		std::string vertexSrc =R"(
+			#version 330 core
+			layout(location=0) in vec3 a_Position;
+			out vec3 v_Position;
+			void main(){
+				v_Position=a_Position;
+				gl_Position = vec4(a_Position,1.0);
+			}
+		)";
+		std::string fragmentSrc =R"(
+			#version 330 core
+			layout(location=0) out vec4 color;
+			in vec3 v_Position;
+			void main(){
+				color=vec4(v_Position*0.5+0.5,1.0);
+			}
+		)";
+
+		m_Shader.reset(new Shader(vertexSrc,fragmentSrc));
+
 	}
 	Application::~Application() {
 
@@ -76,7 +96,8 @@ namespace Engine {
 			glClearColor(0.1f, 0.1f, 0.1f, 1);
 			glClear(GL_COLOR_BUFFER_BIT);
 
-			glUseProgram(0);
+			//glUseProgram(0);
+			m_Shader->Bind();
 			glBindVertexArray(m_VertexArray);
 			glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
 			for (Layer* layer : m_LayerStack)	layer->OnUpdate();
--- GameEngine/src/Engine/Application.h
+++ GameEngine/src/Engine/Application.h
@@ -8,6 +8,8 @@
 
 #include "Engine/imGui/imGuiLayer.h"
 
+#include "Engine/Renderer/Shader.h"
+
 namespace Engine {
 
 	class ENGINE_API Application{
@@ -32,6 +34,7 @@ namespace Engine {
 		LayerStack m_LayerStack;
 
 		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		std::unique_ptr<Shader> m_Shader;
 	private:
 		static Application* s_Instance;
 	};
